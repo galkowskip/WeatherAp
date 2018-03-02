54ac48e19a0dec0f6c4f5e3c2f62262b3e973b15
Paweł Gałkowski <galkowski.p97@gmail.com>
2018-03-02 09:58:02 +0100

    input gui

--- public/script/view.js
+++ public/script/view.js
@@ -1,7 +1,7 @@
 function viewInput() {
     var html = '<form class="form"><textarea class="input"></textarea><button class="btnInput">Input</button></form>'
-    html = parseHTML(html)
-    $('.content').append(html);
+    html = jQuery.parseHTML(html)
+    $('.content').append(html)
 }
 function clear() {
     $('.content').empty()
