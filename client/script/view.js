2a349eb107a0a30d0d45820f398cd51baa630a9e
Paweł Gałkowski <galkowski.p97@gmail.com>
2018-03-05 16:24:45 +0100

    Small screen support

--- client/script/view.js
+++ client/script/view.js
@@ -6,7 +6,7 @@ function getDate(i) {
 }
 function viewWeather(res) {
     for(var i = 0; i < 8; i++) {
-        const html = '<div class="wbox"><div class="ico">' + getDate(i) + '</div><div class="wtext"><div class="temperatureHigh">' + res.week[i].temperatureHigh + '°C</div><div class="temperatureLow">' + res.week[i].temperatureLow + '°C</div><div class="wind"></div><div class="summary">' + res.week[i].summary + '</div></div></div>'
+        const html = '<div class="wbox"><div class="ico">' + res.week[i].icon + '</div><div>' + getDate(i) + '</div><div class="wtext"><div class="temperatureHigh">' + res.week[i].temperatureHigh + '°C</div><div class="temperatureLow">' + res.week[i].temperatureLow + '°C</div><div class="wind"></div><div class="summary">' + res.week[i].summary + '</div></div></div>'
         $('#content').append(html);
     }
 }
