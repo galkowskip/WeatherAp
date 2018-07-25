0031efd85d81b0c2ea95bbf98411c250279051d2
Paweł Gałkowski <galkowski.p97@gmail.com>
2018-07-25 11:04:59 +0200

    fix #1

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3001;
+const port = process.env.PORT;
 const morgan = require('morgan')
-const session = require('express-sessions')
 
 //Ejs configuration
 
